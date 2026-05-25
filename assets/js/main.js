7653806d5e60e5d91a3c7d6ac7978bc70580a619
rhnbztnl <rhnbztnl@gmail.com>
2026-05-25 23:52:21 +0700

    fix: arrow up not working as it should

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,7 +129,7 @@ window.addEventListener('scroll', scrollActive)
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () =>{ 
    const scrollUp = document.getElementById('scroll-up') 
-   window.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+   window.scrollY >= 450 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
 } 
 window.addEventListener('scroll', scrollUp)
 
